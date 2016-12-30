36ac41e0b79bb00fdf5e9cae9e1312094b8a2b39
Soumyabrata Dev <Soumyabrata@users.noreply.github.com>
2016-12-30 15:26:46 +0800

    Updated with comments

--- roughSetScripts/positive_region.m
+++ roughSetScripts/positive_region.m
@@ -16,23 +16,25 @@ function [POS_att , gamma_att] =  positive_region(decision_table , attribute_num
 % (b) gamma_att = Gamma value of the particular attribute queried.
 
 
-    
+    % Find the size of the decision table. The variable "tot_len" indicates the number of observations.
     [tot_len,~] = size(decision_table) ;
     
+    % Generate the corresponding partitions
     IND_att =  indisc_att(decision_table , attribute_number) ;
     IND_decision =  indisc_att(decision_table , dec_attribute_number) ;
     
-    
-    POS_al = [];
+    % This array contains all the observations that fall under positive region.
+    POS_al = [];    % Initialization
 
-    for ent = 1:length(IND_att)
-        A = IND_att{ent,1};
+    for ent = 1:length(IND_att) % We loop for all sets of the partition contained in the cell array.
+        A = IND_att{ent,1}; % Corresponding set of the cell array.
 
-        for t = 1:length(IND_decision)
+        for t = 1:length(IND_decision)  % We check "A" against all sets of the decision partition.
             B = IND_decision{t,1};
-            Lia = ismember(A,B);
+            Lia = ismember(A,B);    % Returns a logical "1" if data of A is found in B
 
             temp = any(Lia==0);
+            % This is the case when "Lia" is all 1's. That means all elements of A are found in B; which in fact means "positive region".
             if temp == 0
                 POS_al = cat(1,POS_al,A);
             end
